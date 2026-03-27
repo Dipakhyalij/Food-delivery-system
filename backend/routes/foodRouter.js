b731ee6f07f2eeebc797c4f7beadbcca286aa5ec
Dipak <dipakpatil200412@gmail.com>
2026-03-27 22:12:07 +0530

    added admin pannel and backend

--- backend/routes/foodRouter.js
+++ backend/routes/foodRouter.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import { addFood, listFood, removeFood } from '../controllers/foodController.js';
 import multer from "multer";
+import fs from "fs";
 
 const foodRouter = express.Router();
 
+// create uploads folder if not exists
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
 // image storage Engine
 const storage = multer.diskStorage({
-  destination: "uploads",
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
   filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}_${file.originalname}`);
+    cb(null, `${Date.now()}_${file.originalname}`);
   }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 foodRouter.post('/add', upload.single("image"), addFood);
 foodRouter.get("/list", listFood);
